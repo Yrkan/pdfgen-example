35ca386ee4004e8133a51e15f0f43a1eeb45e003
Thami Memel <memelthami@gmail.com>
2020-12-06 12:06:09 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.14
+
+require github.com/Yrkan/pdfgen v0.0.28
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Yrkan/pdfgen"
+)
+type Data struct {
+	Date string
+	PG1Name	string
+	PG2Name string
+	PG1Street	string
+	PG2Street string
+	PG1CityState string
+	PG2CityState string
+	PG1DOB string
+	PG2DOB string
+	PG1SSN string
+	PG2SSN string
+	PG1Ownership string
+	PG2Ownership string
+
+}
+func main() {
+	t1 := Data{
+		Date: "11/11/2020", 
+		PG1Name: "name",
+		PG2Name: "Mark",
+		PG1Street: "Street",
+		PG2Street: "13 NY street",
+		PG1CityState: "Rabat",
+		PG2CityState: "Morocco",
+		PG1DOB: "DOB1",
+		PG2DOB: "DOB2",
+		PG1SSN: "1234",
+		PG2SSN: "4567",
+		PG1Ownership: "33%",
+		PG2Ownership: "93%",
+	}
+
+	// Write Example
+	f, _ := os.Create("./testIoWriter.pdf")
+	pdfgen.Write("./input/Template.docx", t1, f)
+	f.Close()
+	
+	// Save Example
+	pdfgen.Save("./input/Template.docx", t1, "./output/conv.pdf")
+	
+}
